12c9188e13a46c130378bd41e873bc9e9f55477b
Arthur Jen <cowboy5110@hotmail.com>
2015-06-26 00:47:27 -0400

    Add default config

---
+++ default-config.py
@@ -0,0 +1,15 @@
+from db import db_config
+
+class BasicConfig(object):
+    SQLALCHEMY_DATABASE_URI = db_config.SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_MIGRATE_REPO = db_config.SQLALCHEMY_MIGRATE_REPO
+
+class APIDevConfig(BasicConfig):
+    DEBUG = True
+    SERVER_NAME = 'localhost:5050'
+
+# TODO: Enable production in a bit.
+#
+# class APIProductionConfig(BasicConfig):
+#     DEBUG = False
+#     SERVER_NAME = 'www.arthur-jen.com'
