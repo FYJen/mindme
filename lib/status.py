6141eb24f17232252768505bd93411960835635e
Arthur Jen <cowboy5110@hotmail.com>
2015-06-26 15:19:26 -0400

    Create general lib/. currently it contains status reporting class

---
+++ lib/status.py
@@ -0,0 +1,36 @@
+"""Custom Status.
+"""
+class CustomStatus(Exception):
+    """
+    """
+    def __init__(self, msg=None, details={}, result={}):
+        self.statusCode = self.__class__.__name__
+        self.statusMsg = msg
+        self.statusDetails = details
+        self.result = result
+
+    def toDict(self):
+        return {
+            'result': self.result,
+            'status': {
+                'statusCode': self.statusCode,
+                'statusMsg': self.statusMsg,
+                'statusDetails': self.statusDetails
+            }
+        }
+
+class HTTPOk(CustomStatus):
+    def __init__(self, msg='OK', **kwargs):
+        CustomStatus.__init__(self, msg=msg, **kwargs)
+
+class ResourceNotFound(CustomStatus):
+    def __init__(self, msg='Resource not found', **kwargs):
+        CustomStatus.__init__(self, msg=msg, **kwargs)
+
+class InvalidRequest(CustomStatus):
+    def __init__(self, msg='Inavlid request', **kwargs):
+        CustomStatus.__init__(self, msg=msg, **kwargs)
+
+class InternalServerError(CustomStatus):
+    def __init__(self, msg='Internal server error', **kwargs):
+        CustomStatus.__init__(self, msg=msg, **kwargs)
