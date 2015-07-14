dbaecf274d54703cfc812db3dd6f2c2b180a1c87
Arthur Jen <cowboy5110@hotmail.com>
2015-07-14 18:20:37 -0400

    Update temporary return values from api

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,1 +1,2 @@
 from app.views import reminder
+from app.views import user
--- app/views/reminder.py
+++ app/views/reminder.py
@@ -5,6 +5,16 @@ from app import mindme_api
 from lib import status
 
 
-@mindme_api.route('/api/v1/reminder/', methods=['GET'])
-def reminder_find():
-    return 'testing'
+@mindme_api.route('/api/v1/reminder/get/', methods=['GET'])
+def reminder_get():
+    return 'reminder_find'
+
+
+@mindme_api.route('/api/v1/reminder/create', methods=['POST'])
+def reminder_create():
+    return 'reminder_create'
+
+
+@mindme_api.route('/api/v1/reminder/update', methods=['POST'])
+def reminder_update():
+    return 'reminder_update'
