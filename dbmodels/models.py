b0856b574c85038283627b28784d5bd8b4010f93
Arthur Jen <cowboy5110@hotmail.com>
2015-07-14 18:21:56 -0400

    Added first version of db model

---
+++ dbmodels/models.py
@@ -0,0 +1,85 @@
+from app import db
+
+
+class ReceivedMessage(db.Model):
+    """
+    """
+    __tableame__ = 'received_message'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message_id = db.Column(
+        db.Integer,
+        db.ForeignKey('message.id'),
+        nullable=False
+    )
+
+
+class SentMessage(db.Model):
+    """
+    """
+    __tableame__ = 'sent_message'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message_id = db.Column(
+        db.Integer,
+        db.ForeignKey('message.id'),
+        nullable=False
+    )
+
+
+class Message(db.Model):
+    """
+    """
+    __tableame__ = 'message'
+    message = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime)
+    status_id = db.Column(
+        db.Integer,
+        db.ForeignKey('status.id'),
+        nullable=False
+    )
+
+    rcv_user_mapping = db.relationship(
+        'ReceivedMessage',
+        backref='message',
+        uselist=False
+    )
+    sent_user_mapping = db.relationship(
+        'SentMessage',
+        backref='message',
+        uselist=False
+    )
+
+
+class Status(db.Model):
+    """
+    """
+    __tableame__ = 'status'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    messages_with_status = db.relationship(
+        'Message',
+        backref='status',
+        lazy='dynamic'
+    )
+
+
+class User(db.Model):
+    """
+    """
+    __tableame__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    fb_id = db.Column(db.String(64), nullable=False, index=True, unique=True)
+    gcm_id = db.Column(db.String(128), nullable=False, unique=True)
+
+    rcv_messages = db.relationship(
+        'ReceivedMessage',
+        backref='assignee',
+        lazy='dynamic'
+    )
+    sent_messages = db.relationship(
+        'SentMessage',
+        backref='author',
+        lazy='dynamic'
+    )
