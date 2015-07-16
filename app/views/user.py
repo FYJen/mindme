d589c6f61a2424caa2bf20bc84b68a46bf653fb3
Arthur Jen <cowboy5110@hotmail.com>
2015-07-16 13:56:33 -0400

    Bootstrapped user api endpoint

--- app/views/user.py
+++ app/views/user.py
@@ -6,12 +6,30 @@ from lib import status as custom_status
 from resources import User
 
 
+@mindme_api.route('/api/v1/user/get/<int:user_id>/', methods=['GET'])
+def user_get(user_id):
+    """
+    """
+    try:
+        user = User.get(user_id)
+        result = custom_status.HTTPOk(result=user)
+    except custom_status.CustomStatus as e:
+        result = e
+    # except Exception:
+    #     result = custom_status.InternalServerError()
+
+    return json.dumps(result.toDict())
+
+
 @mindme_api.route('/api/v1/user/login/', methods=['POST'])
 def user_login():
     query_args = {
         'token': request.args.get('token', ''),
         'expiration': request.args.get('expiration', ''),
-        'fb_id': request.args.get('fb_id', '')
+        'fb_id': request.args.get('fb_id', ''),
+        # TODO(ajen):
+        #   1. Update user with real gcm id.
+        'gcm_id': request.args.get('gcm_id', 'tmp_gcm_id')
     }
 
     try:
@@ -27,4 +45,14 @@ def user_login():
 
 @mindme_api.route('/api/v1/user/logout/', methods=['POST'])
 def user_logout():
-    return 'user_logout'
+    token = request.args.get('token', '')
+
+    try:
+        user = User.logout(token)
+        result = custom_status.HTTPOk(result=user)
+    except custom_status.CustomStatus as e:
+        result = e
+    except Exception:
+        result = custom_status.InternalServerError()
+
+    return json.dumps(result.toDict())
--- resources/user.py
+++ resources/user.py
@@ -1,48 +1,99 @@
-from datetime import datetime
-
 from app import db
 from base import API_Base
-from config import time_format
 from dbmodels import models
 from lib import status as custome_status
+from message import Reminder
 
 
 class User(API_Base):
     """
     """
     @classmethod
-    def get(cls):
+    def get(cls, user_id, deref_all=True):
         """
         """
-        pass
+        user_obj = models.User.query.get(user_id)
+        if not user_obj:
+            raise custome_status.ResourceNotFound(
+                details='No user found with the given id - %s' % user_id
+            )
+
+        return cls._to_Dict(user_obj, deref_all)
 
     @classmethod
-    def _create(cls):
+    def _create(cls, fb_id, gcm_id):
         """
         """
-        # TODO(ajen):
-        #   1. Generate gcm_id
-        #   2. Create user in our db
-        pass
+        user_obj = models.User(fb_id=fb_id, gcm_id=gcm_id)
+
+        db.session.add(user_obj)
+        db.session.commt()
+
+        return user_obj
 
     @classmethod
-    def login(cls):
+    def login(
+        cls,
+        token='',
+        expiration='',
+        fb_id='',
+        gcm_id='',
+        deref_all=False
+    ):
         """
         """
+        if not all([token, fb_id, gcm_id]):
+            raise custome_status.InvalidRequest(
+                details='Bad request: Missing token or fb_id'
+            )
+
+        # TODO(ajen): Perhaps we should validate fb access token. Not a big
+        #             deal at the moment.
+
+        # Check if we have the given user in our database. If not, we add it.
+        user_obj = models.User.query.filter_by(fb_id=fb_id).first()
+        if not user_obj:
+            user_obj = cls._create(fb_id, gcm_id)
+
         # TODO(ajen):
         #   1. If user is not in redis, add token to redis.
-        pass
+        #   2. Set expiration time on the cache.
+        return cls._to_Dict(user_obj, deref_all)
 
     @classmethod
-    def logout(cls):
+    def logout(cls, token):
         """
         """
+        if not token:
+            raise custome_status.InvalidRequest(
+                details='Bad request: Missing token'
+            )
+
         # TODO(ajen):
         #   1. If user is in redis, invalidate the cache.
         pass
 
     @classmethod
-    def _to_Dict(cls):
+    def _to_Dict(cls, user, deref_all, *args, **kwargs):
         """
         """
-        pass
+        # Note: we do not return user's gcm_id.
+        user_dict = {
+            'id': user.id,
+            'fb_id': user.fb_id,
+            'gcm_id': user.gcm_id
+        }
+
+        if deref_all:
+            user_dict.update({
+                'received_messages': [
+                    Reminder._to_Dict(msg.message, deref_all)
+                    for msg in user.rcv_messages
+                ],
+                'sent_messages': [
+                    Reminder._to_Dict(msg.message, deref_all)
+                    for msg in user.sent_messages
+                ]
+            })
+
+        return user_dict
